b99f24ef5d3a853ed578746159e5491263373db7
kamalkj9460 <79216365+kamalkj9460@users.noreply.github.com>
2022-11-22 22:51:37 +0530

    Original Theme
    Original Theme

---
+++ assets/bold-checkout-options.js
@@ -0,0 +1,2 @@
+window.BOLD = window.BOLD || {};
+window.BOLD.checkout_features_defaults=[{"name":"checkout-always-on","require":false},{"name":"Product Upsell","require":true,"id":"bd418c3c-a207-11e7-9971-42010af00be5"}]
